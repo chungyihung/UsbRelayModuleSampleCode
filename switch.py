30da19d19f6d21ad4b909e2921dac3d0b08381f9
JosephHung <chungihung1988@gmail.com>
2021-10-14 22:52:43 +0800

    Sample code for usb relay module
    The usb relay module is based on CH340 USB to serial port control
    chip. This change implement a simple python class to control each relay.

---
+++ switch.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import codecs
+import serial
+import time
+
+# This is a sample code for http://www.chinalctech.com/cpzx/32.html
+
+DEFAULT_DEVPATH="/dev/ttyUSB0"
+DEFAULT_TIMEOUT=2
+
+class RelayCtrl:
+    def __init__(self, devpath=DEFAULT_DEVPATH, timeout=DEFAULT_TIMEOUT):
+        self.devpath = devpath
+        self.timeout = timeout
+        self.ON_CMD= [b"A00101A2", b"A00201A3", b"A00301A4", b"A00401A5"]
+        self.OFF_CMD=[b"A00100A1", b"A00200A2", b"A00300A3", b"A00400A4"]
+
+    def open(self):
+        self.s = serial.Serial(self.devpath, "9600", timeout=self.timeout)
+        print("open " + self.s.name)
+
+    def turnon(self, index):
+        self.s.write(codecs.decode(self.ON_CMD[index], "hex"))
+        time.sleep(0.1)
+
+    def turnoff(self, index):
+        self.s.write(codecs.decode(self.OFF_CMD[index], "hex"))
+        time.sleep(0.1)
+
+    def status(self):
+        self.s.write(codecs.decode(b'FF','hex'))
+        res=self.s.read(4)
+        print(res)
+
+    def close(self):
+        self.s.close()
+
+# Test sequence
+myrelay = RelayCtrl()
+
+myrelay.open()
+myrelay.turnon(0)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnon(1)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnon(2)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnon(3)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnoff(3)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnoff(2)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnoff(1)
+myrelay.status()
+time.sleep(0.5)
+myrelay.turnoff(0)
+myrelay.status()
