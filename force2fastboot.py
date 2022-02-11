0dd98a9a6ea5e62138cf253b716a0804b1491180
JosephHung <chungihung1988@gmail.com>
2022-02-11 14:12:59 +0800

    Add script to force reboot device to fastboot

---
+++ force2fastboot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import codecs
+import serial
+import time
+from switch import RelayCtrl
+
+POWER_PIN = 3
+VOL_DOWN_PIN = 2
+
+# Test sequence
+myrelay = RelayCtrl()
+
+myrelay.open()
+
+# Turn off all relays
+myrelay.turnoff(0)
+myrelay.turnoff(1)
+myrelay.turnoff(VOL_DOWN_PIN)
+myrelay.turnoff(POWER_PIN)
+
+# Emulate power key press 16s to force shutdown device
+myrelay.turnon(POWER_PIN)
+myrelay.status()
+time.sleep(16)
+myrelay.turnoff(POWER_PIN)
+myrelay.status()
+time.sleep(0.5)
+
+# Emulate Vol down key press to make device reboot to fastboot mode
+myrelay.turnon(VOL_DOWN_PIN)
+time.sleep(2.5)
+myrelay.turnoff(VOL_DOWN_PIN)
